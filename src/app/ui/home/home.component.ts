2974ca1c221579d4d4371e03018591c3c77bd5b5
Shantanu Bagchi <shantanubagchi@shantanus-mbp.myfiosgateway.com>
2020-10-11 17:07:13 -0400

    new data and promo

--- src/app/ui/home/home.component.ts
+++ src/app/ui/home/home.component.ts
@@ -12,19 +12,14 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
   slides = [
-    "../assets/images/promo/slide1.png", 
-    "../assets/images/promo/slide2.png", 
-    "../assets/images/promo/slide3.png", 
-    "../assets/images/promo/slide4.png", 
-    "../assets/images/promo/slide5.png", 
-    "../assets/images/promo/slide6.png", 
-    "../assets/images/promo/slide7.png", 
-    "../assets/images/promo/slide8.png", 
-    "../assets/images/promo/slide9.png", 
-    "../assets/images/promo/slide10.png", 
-    "../assets/images/promo/slide11.png", 
-    "../assets/images/promo/slide12.png", 
-    "../assets/images/promo/slide13.png"
-
+    "../assets/images/promo/Day 1.png", 
+    "../assets/images/promo/Day 2.png", 
+    "../assets/images/promo/Day 3.png", 
+    "../assets/images/promo/Day 4.png", 
+    "../assets/images/promo/Day 5.png", 
+    "../assets/images/promo/Day 6.png", 
+    "../assets/images/promo/Day 7.png", 
+    "../assets/images/promo/Slide 8.png", 
+    "../assets/images/promo/Slide 9.png"
   ]
 }
